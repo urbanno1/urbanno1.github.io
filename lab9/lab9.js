7c928b083c5c11c9445510fbec7618449d838fc4
Chinedu <eduurbanus42@gmail.com>
2019-09-12 23:25:59 -0500

    submit form working

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -2,29 +2,35 @@
 
 $(function() {
 
-    $('#new_ticket').submit(function(e) {
+    $('#new_ticket').on('click', function(e) {
         e.preventDefault();
 
         const error = $('#error');
+        error.html('');
+        let submitFlag = true;
+
         let name = $("#name").val();
         let street = $("#street").val();
         let seat = $("#seat").val();
 
-        let seat_regex =  '/^[0-9] $/'
 
         if(name == '') {
+            submitFlag = false;
             error.append(
                 $("<p>",  { text: "Name cannot be null!", style: "color:red"})
             )
         }
 
-        let test = seat_regex.test(seat);
-        if(!test || seat >= 200) {
+        if(seat  <= 0 || seat >=200) {
+            submitFlag = false;
             error.append(
                 $("<p>", { text: "Seat number must be a number less than 200!", style: "color:red"})
             )
         }
 
+        if(submitFlag) {
+            this.submit();
+        }
 
     })
 });
